c1ae9836a0e03767ffd851f3a7b443a5cc61dfb8
Elham Alipour <136172033+elialipour@users.noreply.github.com>
2023-09-20 14:29:53 +0330

    Initial commit

---
+++ src/Cities.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+export default function Cities() {
+  return (
+    <div className="cities">
+      <ul>
+        <li>Sydney</li>
+        <li>Paris</li>
+        <li>Montreal</li>
+        <li>Lisbon</li>
+        <li>San Francisco</li>
+      </ul>
+    </div>
+  );
+}
